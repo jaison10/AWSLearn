394b6c65ee3bb38abd49edffcc134a619a1975f3
jaison10 <jaisonj1010@gmail.com>
2023-11-18 22:29:38 +0000

    message sent

--- SQSTest/SQSTest/Program.cs
+++ SQSTest/SQSTest/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SQSResponse;
+using System.Text.Json;
+
+var client = new AmazonSQSClient();
+
+var studentDetails = new StudentCreation
+{
+    Id = new Guid(),
+    FullName = "John Cena",
+    DOB = new DateTime(2000, 02, 13),
+    Email = "johncena@gmail.com",
+    GitHubUserName = "johncena"
+};
+
+var queueURLResponse = await client.GetQueueUrlAsync("Customer-London");
+
+var sendMsgReq = new SendMessageRequest
+{
+    QueueUrl = queueURLResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(studentDetails)
+};
+
+var sentMsgResponse = await client.SendMessageAsync(sendMsgReq);
+
+Console.WriteLine(sentMsgResponse);
---
+++ SQSTest/SQSTest/StudentCreation.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SQSResponse
+{
+    public class StudentCreation
+    {
+        public required Guid Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string GitHubUserName { get; set; }
+        public required DateTime DOB { get; set; }
+    }
+}
